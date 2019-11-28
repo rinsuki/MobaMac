c995b0c2c65472ee8c210693a0a272b16cbef020
rinsuki <428rinsuki+git@gmail.com>
2019-11-28 12:30:10 +0900

    Initial Commit

---
+++ MobaMac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MobaMac
+//
+//  Created by user on 2019/11/28.
+//  Copyright © 2019 rinsuki. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MobaMac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MobaMac
+//
+//  Created by user on 2019/11/28.
+//  Copyright © 2019 rinsuki. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
